887f4db0ad4d7baeb1008460aec57869c6a4faaf
loovedoceria-web <loovedoceria@gmail.com>
2026-07-29 15:50:07 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from "./supabaseClient";
 
 const styles = {
   page: { display: 'flex', flexDirection: 'column', gap: 18, fontFamily: 'inherit' },
