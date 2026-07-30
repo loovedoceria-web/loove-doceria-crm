82b2994c9b6cf9e7d6a86db3fd919ce3057a76b2
loovedoceria-web <loovedoceria@gmail.com>
2026-07-30 12:23:27 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-      format: {
-        comments: false,
-      },
-    },
+    minify: 'esbuild', // Usa o minificador padrão nativo do Vite
   },
 })
